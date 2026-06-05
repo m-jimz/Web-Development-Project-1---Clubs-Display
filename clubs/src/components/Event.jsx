52fbfe19309e73947f985865ba01eeda78942cbf
Marco <marcjimenez790@gmail.com>
2026-06-05 00:27:59 -0500

    Add Event component

---
+++ clubs/src/components/Event.jsx
@@ -0,0 +1,11 @@
+const Event = (props) => {
+  return (
+    <div className="Event">
+      <h5>{props.title}</h5>
+      <h6>{props.description}</h6>
+      <a href={`mailto:${props.email}`}>Email!</a>
+    </div>
+  );
+};
+
+export default Event;
