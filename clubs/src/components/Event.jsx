2855daac1e8c372613f750a11bbab99bea0bfc63
Marco <marcjimenez790@gmail.com>
2026-06-08 19:00:35 -0500

    Adding files

--- clubs/src/components/Event.jsx
+++ clubs/src/components/Event.jsx
@@ -1,6 +1,6 @@
 const Event = (props) => {
   return (
-    <div className="Event">
+    <div className="Event">   
       <h5>{props.title}</h5>
       <h6>{props.description}</h6>
       <a href={`mailto:${props.email}`}>Email!</a>
