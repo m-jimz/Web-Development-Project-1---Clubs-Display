a1fd2744888973cd3cd8f959042af0d913bb30a9
Marco <marcjimenez790@gmail.com>
2026-06-09 22:59:34 -0500

    Update css files

--- clubs/src/App.jsx
+++ clubs/src/App.jsx
@@ -5,8 +5,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Title</h1>
-      <h2>Description</h2>
+      <h1>Clubs</h1>
+      <h2>Here is all the clubs and contact information!</h2>
       <Card />
       
     </div>
